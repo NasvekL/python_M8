cdfcd578fb538a1246ce9999a7f3657ddc248c38
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2026-03-05 21:43:41 -0300

    cambios

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.user import User
-
-
-async def get_all(db: AsyncSession) -> list[User]:
-    result = await db.execute(select(User))
-    return list(result.scalars().all())
-
-
-async def get_by_id(db: AsyncSession, user_id: int) -> User | None:
-    result = await db.execute(select(User).where(User.id == user_id))
-    return result.scalar_one_or_none()
-
-
-async def get_by_username(db: AsyncSession, username: str) -> User | None:
-    result = await db.execute(select(User).where(User.username == username))
-    return result.scalar_one_or_none()
-
-
-async def get_by_email(db: AsyncSession, email: str) -> User | None:
-    result = await db.execute(select(User).where(User.email == email))
-    return result.scalar_one_or_none()
-
-
-async def create(db: AsyncSession, username: str, email: str) -> User:
-    user = User(username=username, email=email)
-    db.add(user)
-    await db.commit()
-    await db.refresh(user)
-    return user
-
-
-async def update(db: AsyncSession, user: User) -> User:
-    await db.commit()
-    await db.refresh(user)
-    return user
-
-
-async def delete(db: AsyncSession, user: User) -> None:
-    await db.delete(user)
-    await db.commit()
-=======
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -86,4 +42,3 @@ async def update(db: AsyncSession, user: User) -> User:
 async def delete(db: AsyncSession, user: User) -> None:
     await db.delete(user)
     await db.commit()
->>>>>>> origin/master
